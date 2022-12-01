4d11ce5b39458c9f067e7b47add6fd2678790fae
Александр Биктеев <bikteev@mail.ru>
2022-12-01 15:45:32 +0600

    Initial Commit

---
+++ ContactListSUI/ContactListSUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContactListSUIApp.swift
+//  ContactListSUI
+//
+//  Created by albik on 01.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct ContactListSUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
